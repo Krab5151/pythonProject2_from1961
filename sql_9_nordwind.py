7067dde662198907b7384cb5692ca4587b026e2c
Kirill <julich36ru@gmail.com>
2024-10-28 23:59:10 +0300

    28 10 254 Data Base

---
+++ sql_9_nordwind.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+
+# Подключение к базе данных
+conn = psycopg2.connect(
+    dbname="Eduson",  # имя базы данных
+    user="student",  # ваш логин
+    password="qweasd1234567890",  # ваш пароль
+    host="185.86.147.205",  # хост, например, localhost
+    port="5432"  # порт, стандартный для PostgreSQL
+)
+
+# Создаем курсор для выполнения SQL запросов
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM shippers")
+print(cur.fetchall())
