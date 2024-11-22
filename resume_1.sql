d42f1c89f81a472572f39f5c6f10e0cc841e7169
Kirill <julich36ru@gmail.com>
2024-11-22 13:37:48 +0300

    22 11 24 Window Function, Interval

--- resume_1.sql
+++ resume_1.sql
@@ -30,11 +30,12 @@ FROM
 --С использованием оконной функции
 '
 Объяснение:
-RANK():
+RANK() - Ранжирует Только аргумент из ORDER BY и Выполняется в Последнюю очередь
 --Используется для ранжирования строк в каждой группе (по годам).
 --Параметр PARTITION BY to_char(ord_datetime, ''YYYY'') делит данные на группы по годам.
 --ORDER BY COUNT(ord_an) DESC упорядочивает строки по убыванию cnt в каждой группе.
-WHERE rnk = 1: Оставляет только строки с максимальным значением cnt в каждом году.
+WHERE rnk = 1: Так Сортировка по Убыванию, то ранжир № 1 соответствует максимальному значению COUNT(ord_an)' ||
+'Оставляет только строки с максимальным значением cnt в каждом году.
 ORDER BY year: Упорядочивает итоговые строки по годам.
 '
 SELECT year, an_id, cnt
@@ -110,8 +111,102 @@ SELECT *
 FROM analysis_results
 WHERE cnt = 10; -- Замените 10 на нужное значение
 
+--------------------------------
+ WITH  sq_1 AS(
+   SELECT to_char(ord_datetime, 'YYYY') as year, an_id, COUNT(ord_an) as cnt
+    FROM analysis a
+    RIGHT JOIN orders o
+    ON an_id = ord_an
+    GROUP BY an_id, year
+    ORDER BY year
+ ),
+ sq_2 AS(
+ SELECT MAX(cnt)
+      FROM  (
+        SELECT to_char(ord_datetime, 'YYYY') as year, COUNT(ord_an) as cnt
+        FROM analysis a
+        RIGHT JOIN orders o
+        ON an_id = ord_an
+        GROUP BY an_id, to_char(ord_datetime, 'YYYY')
+        HAVING to_char(ord_datetime, 'YYYY') = sq_1.year )
+ ) AS sq_3
+ SELECT *
+   FROM sq_2
+ ;
+-- Исправленный вариант
+WITH
+  sq_1 AS (
+    SELECT to_char(ord_datetime, 'YYYY') as year, an_id, COUNT(ord_an) as cnt
+    FROM analysis a
+    RIGHT JOIN orders o
+    ON an_id = ord_an
+    GROUP BY an_id, to_char(ord_datetime, 'YYYY')
+  ),
+  sq_2 AS (
+    SELECT year, MAX(cnt) AS max_cnt
+    FROM sq_1
+    GROUP BY year
+  )
+SELECT sq_1.year, sq_1.an_id, sq_1.cnt
+FROM sq_1
+JOIN sq_2
+ON sq_1.year = sq_2.year AND sq_1.cnt = sq_2.max_cnt
+ORDER BY sq_1.year;
+'////////////////////////////////////'
 
+ WITH  sq_1 AS(
+   SELECT to_char(ord_datetime, 'YYYY') as year, an_id, COUNT(ord_an) as cnt
+    FROM analysis a
+    RIGHT JOIN orders o
+    ON an_id = ord_an
+    GROUP BY an_id, year
+    ORDER BY year
+ ),
+ sq_2 AS(
+      SELECT MAX(cnt)
+      FROM sq_1
+ )
+ SELECT *
+ FROM sq_2
+;
 
+--//////////////////////////////////////////////////////
+--/////////////////////////////////////////////////////
+SELECT res.year, res.an_id, res.cnt, RANK() OVER(PARTITION BY res.year ORDER BY res.cnt DESC) as rnk
+FROM
+  (SELECT to_char(ord_datetime, 'YYYY') as year,
+        an_id,
+        COUNT(an_id) as cnt
+  from orders o
+  left JOIN analysis a
+  ON an_id = ord_an
+  GROUP BY to_char(ord_datetime, 'YYYY'), an_id
+  ORDER BY to_char(ord_datetime, 'YYYY')
+  ) as res
+;
 
+SELECT res.year, res.an_id, res.cnt, RANK() OVER(PARTITION BY res.year ORDER BY res.cnt DESC) as rnk
+FROM
+  (SELECT to_char(ord_datetime, 'YYYY') as year,
+        an_id,
+        COUNT(an_id) as cnt
+  from orders o
+  left JOIN analysis a
+  ON an_id = ord_an
+  GROUP BY to_char(ord_datetime, 'YYYY'), an_id
+  ORDER BY to_char(ord_datetime, 'YYYY')
+  ) as res
+;
 
-
+WITH ranked_orders AS (
+    SELECT
+        to_char(ord_datetime, 'YYYY') as year,
+        ord_an,
+        RANK() OVER(PARTITION BY to_char(ord_datetime, 'YYYY')
+        ORDER BY COUNT(ord_id) DESC ) as rnk
+    FROM orders
+    GROUP BY to_char(ord_datetime, 'YYYY'), ord_an
+)
+SELECT ranked_orders.year, ranked_orders.rnk
+FROM  ranked_orders
+WHERE rnk = 1;
