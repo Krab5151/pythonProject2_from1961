87016357b71c0912a660633171c5560096c73f70
Kirill <julich36ru@gmail.com>
2025-03-04 10:06:21 +0300

    04 03 2025 re rule Правила для регулярки с числами

--- re_re_rule.py
+++ re_re_rule.py
@@ -1,6 +1,68 @@
 
 import re
 from bs4 import BeautifulSoup
+
+
+# TODO Правило:
+#  Даты и Телефоны группируются по заданным разделителям / или -  (dd/mm/yyyy или 8-ххх-хх-хх)
+#  Каждая группа состоит из разрядов
+#  Для каждого разряда диапазон только [0-9],
+#  {4} - Фигурные скобки это кол-во повторов Цифры
+
+
+# TODO Задача: извлечь корректные даты и телефоны
+text_data = '''Сегодня на календаре 02/03/2025, а вчера было 01/03/2025.  
+Встреча запланирована на 15/04/2024, а отчёт сдадим до 30/06/2023.  
+Некорректные даты: 99/99/9999, 32/13/2025, 7/3/2024.  
+Допустим, в логе есть 05/08/2020 и 25/12/2019.'''
+
+
+phones = """+7 909-212-9930", "+3*9272160048", "7^999999a", +7 910-212-9930, +9 920-216-00-48, 8 920-216-0048"""
+
+
+# TODO Жёсткая Примитивная Регулярка
+res = "".join(re.findall(r"\d+\W", text_data))
+res1 = "".join(re.findall(r"[\d+/ ]", text_data))
+
+
+print(res, '\n')
+print([i for i in res1.split(" ") if i[0:2] < "32" and i[3:6] < "13" and i[7:] < "2025" and i != ""], '\n')
+
+
+#  TODO Костыли, c помощью итерации удаляемых символов
+res2 = []
+for p in ".,":
+    text_data = text_data.replace(p, " ")
+data = text_data.split(" ")
+for i in data:
+    if i[0:2] < "32" and i[3:6] < "13" and i[7:] < "2025" and i != "":
+        res2.append(i)
+print(res2, '\n')
+
+
+# TODO Вывлд даты
+res3 =re.findall(r'[0-9]{2}[^0-9][0-9]{2}[^0-9][0-9]{4}', text_data)
+res4 =re.findall(r'\d{2}\W\d{2}\W\d{4}', text_data)
+res5 =re.findall(r'\b(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}\b', text_data)
+print(res3, '\n')
+print(res4, '\n')
+print(res5, '\n')
+
+
+# TODO Вывод телефона
+res6 = re.findall(r'(?:\+7|8) (?:9[012][09]-)(?:2[01][026]-)(?:\d{4})', phones)
+res7 = re.findall(r'(?:\+7|8)[\s-]?(9\d{2})[\s-]?(\d{3})[\s-]?(\d{2})[\s-]?(\d{2})', phones)
+print(res6, '\n')
+print(res7, '\n')
+
+# TODO Преобразование списка кортежей res7, в удобочитаемый формат
+n = 0
+while n < len(res7):
+    n += 1
+    r = '-'.join([i for i in res7[n-1]])
+    print(r)
+
+
 s = 'aC/DCAC/DCAC/DCAC/DCAC/DCAC/DCAC/DCAC/DCAC/DCAC/DC'
 
 res = re.match('AC/DC', s, flags=re.I) # поиск подстроки В НАЧАЛЕ СТРОКИ s
@@ -147,3 +209,4 @@ def func(match):
 s = " test_sub 0 test_sub 1 test_sub 2 test_sub 3 test_sub 4 "
 res = re.sub( r"(test_sub) (\d)", func,  s)
 print(res, " Поиск и просто замена чисел в строке")
+
